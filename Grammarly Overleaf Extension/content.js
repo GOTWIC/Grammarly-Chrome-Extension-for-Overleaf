f9065acd7cb8b66f6321a7921b30fc9c0a01ef1e
GOTWIC <50720527+GOTWIC@users.noreply.github.com>
2023-01-06 15:24:41 -0500

    Add files via upload

--- Grammarly Overleaf Extension/content.js
+++ Grammarly Overleaf Extension/content.js
@@ -1,3 +1,8 @@
+//import * as Grammarly from "@grammarly/editor-sdk";
+//await Grammarly.init("client_HTtf1BP4pP1a81RGthqbjE");
+
+
+
 chrome.runtime.onMessage.addListener(function (response, sendResponse) {
   grammify()
   return true
@@ -16,11 +21,32 @@ function getRawHTML(){
   return rawHTML
 }
 
+function sendToGrammarly(str){
+  var elem = document.createElement("textarea");
+  elem.id = "grammarly_text_area"
+  elem.innerHTML = str
+  elem.rows = "30"
+  elem.cols = "60"
+  //document.body.appendChild(elem);  
+
+  var gscript = document.createElement("script")
+  gscript.type = "module"
+  gscript.src = "https://unpkg.com/@grammarly/editor-sdk?clientId=client_HTtf1BP4pP1a81RGthqbjE"
+  var gwrapper = document.createElement("grammarly-editor-plugin");
+  gwrapper.appendChild(elem)
+  
+  document.body.appendChild(gscript)
+  document.body.appendChild(gwrapper)
+}
 
 function grammify() {
-  var rawHTML = getRawHTML()
-  var cleanHTML = cleanRawHTML(rawHTML)
+  //var rawHTML = getRawHTML()
+  //var cleanHTML = cleanRawHTML(rawHTML)
+  cleanHTML = "Mispellings and grammatical errors can effect your credibility. The same goes for misused commas, and other types of punctuation . Not only will Grammarly underline these issues in red, it will also showed you how to correctly write the sentence." 
+  sendToGrammarly(cleanHTML)
 }
+
+
 
 
 
